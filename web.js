58841b843f2f29f3d86605398844ed852f11382a
Keshava Annamalai <keshava.a@gmail.com>
2013-07-05 03:23:03 -0700

    modified web.js to include fs

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   var buffer = fs.readFileSync('index.html');
   var helloTxt = buffer.toString('utf8');
   //response.send('Hello World 2!');
