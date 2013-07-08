361fa5e0ea5d10bbdccfee2f51ad6da80e27107b
Keshava Annamalai <keshava.a@gmail.com>
2013-07-08 06:01:27 -0700

    removing url console log stmt

--- grader.js
+++ grader.js
@@ -95,7 +95,7 @@ if(require.main == module) {
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-u, --url <url_to_html>', 'url to html data', clone(assertUrl), "")
         .parse(process.argv);
-    console.log(program.url);
+
     if(!program.url) {
        var checkJson = checkHtmlFile(program.file, program.checks);
        var outJson = JSON.stringify(checkJson, null, 4);
