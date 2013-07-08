b5142af211ea36ce63c2126d5d95f572b585f46d
Keshava Annamalai <keshava.a@gmail.com>
2013-07-08 05:54:49 -0700

    processing urls

--- grader.js
+++ grader.js
@@ -22,9 +22,11 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +37,31 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrl = function(inurl) {
+    var instr = inurl.toString();
+    if(instr.length <= 0) {
+       console.log("bad url: %s", instr);
+       process.exit(1);
+    }
+    return instr;
+};
+
+var getResponder = function(checksfile, htmlfile) {
+    var responder = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+            process.exit(1);
+        } else {
+            fs.writeFileSync(htmlfile, result);
+            var checkJson = checkHtmlFile(htmlfile, checksfile);
+            var outJson = JSON.stringify(checkJson, null, 4);
+            console.log(outJson);
+            fs.unlinkSync(htmlfile)
+        }
+    };
+    return responder;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -66,10 +93,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_to_html>', 'url to html data', clone(assertUrl), "")
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(program.url);
+    if(!program.url) {
+       var checkJson = checkHtmlFile(program.file, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+    } else {
+       var urlResponder = getResponder(program.checks, "downloa2temp.html");
+       rest.get(program.url).on('complete', urlResponder);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
